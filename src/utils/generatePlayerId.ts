e34f56660e687b10e440601e394a93cfd4e2bd46
Domiflame <adyox9767@gmail.com>
2026-05-31 18:23:12 +0530

    Update player ID format to PLR-MUM

--- src/utils/generatePlayerId.ts
+++ src/utils/generatePlayerId.ts
@@ -2,9 +2,9 @@ export const generatePlayerId = () => {
   const chars =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-  let result = "FFMSC-";
+  let result = "PLR-MUM-";
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 6; i++) {
     result += chars.charAt(
       Math.floor(Math.random() * chars.length)
     );
